c6000569e28a21399f393f51f5d608074e01949a
DBasoco <65093434+DBasoco@users.noreply.github.com>
2020-06-12 23:35:16 -0600

    Add files via upload

---
+++ MTG_gui.py
@@ -0,0 +1,22 @@
+# the module containing the necessary graphical design scripts
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+def window(): # simply learning how to use the system to make a gui
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(100, 100, 1000, 1000)  # xps, yps, width, heigth, (0,0) -- ( , )
+    win.setWindowTitle('Magic Processing Unit')
+
+    label = QtWidgets.QLabel(win) # sets where the label goes
+    label.setText('EYHOA!')
+    label.move(50, 50) # xps, yps from top left corner of window
+
+    win.show()
+    sys.exit(app.exec_())
+
+
+window()
