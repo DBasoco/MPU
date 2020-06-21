750a609e7c2b470f8f4accf1d793d16ce930185c
DBasoco <65093434+DBasoco@users.noreply.github.com>
2020-06-21 14:17:46 -0600

    Add files via upload

--- MTG_gui.py
+++ MTG_gui.py
@@ -2,25 +2,34 @@
 
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
-
-from tkinter import *
-
-# still deciding which interface to use
-
-class Root(Tk)
+import sys, time
 
 
+class MyWindow(QMainWindow):
 
-def window(): # simply learning how to use the system to make a gui
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(100, 100, 1000, 1000)  # xps, yps, width, height, (0,0) -- ( , )
+        self.setWindowTitle('Magic Processing Unit')
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("""Let's walk the planes.""")
+        self.update()  # adjusts label width view
+        self.label.move(350, 500)  # xps, yps
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText('TAP')
+        self.b1.clicked.connect(self.swing)
+
+    def swing(self):
+        self.label.setText('You tapped your card.')
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
+
+def window():  # simply learning how to use the system to make a gui
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(100, 100, 1000, 1000)  # xps, yps, width, heigth, (0,0) -- ( , )
-    win.setWindowTitle('Magic Processing Unit')
-
-    label = QtWidgets.QLabel(win) # sets where the label goes
-    label.setText('EYHOA!')
-    label.move(50, 50) # xps, yps from top left corner of window
+    win = MyWindow()
 
     win.show()
     sys.exit(app.exec_())
